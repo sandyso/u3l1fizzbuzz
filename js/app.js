700502f62fb96a8c6851388621c3bd676c1f092b
sandyso <sandyrasom@gmail.com>
2016-04-28 00:03:01 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {    
+
+    for (var i = 0; i <= 100; i++) {
+        console.log(i);
+    }
+
+$("ul").append("<li>" + item + "</li>")
+
+    if (i % 3) {
+        (i).replaceWith("Fizz")
+
+    } else if (i % 5) {
+    (i).replaceWith("Buzz")
+
+ } else (i % 15) {
+    (i).replaceWith("Fizz Buzz")
+}
+});

0d2ed6f162bb57f27555c384f11b492284e1c1d7
sandyso <sandyrasom@gmail.com>
2016-05-03 23:51:15 -0400

    updated code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
+$(document).ready(function() {
 
-$(document).ready(function () {
 
-    for (var i = 1; i <= 100; i++) {
-        $('.fb-list').append('<li>' + i + '</li>');
+        for (var i = 1; i <= 100; i++) {
+            $('.fb-list').append('<li>' + i + '</li>');
 
-        if (i % 3 === 0) {
-        	$('.fb-list').append('fizz');
+            if (i % 3 === 0) {
+                $('.fb-list').append('fizz');
+            }
+
+            if (i % 5 === 0) {
+                $('.fb-list').append('buzz');
+            }
+
+            if (i % 15 === 0) {
+                $('.fb-list').append('fizzbuzz');
+            }
+
         }
-
-        if (i % 5 === 0) {
-            $('.fb-list').append('buzz');
-        }
-
-        if (i % 15 === 0) {
-            $('.fb-list').append('fizzbuzz');
-        }
-
-    }
 });
